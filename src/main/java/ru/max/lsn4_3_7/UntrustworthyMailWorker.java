1de9f0d2af91f39ba086b9cef9baa381f83da7ea
Maxim Serov <maxim58r@gmail.com>
2020-09-17 11:29:02 +0300

    4.3.7 not performed class UntrustworthyMailWorker complimented pm

--- src/main/java/ru/max/lsn4_3_7/UntrustworthyMailWorker.java
+++ src/main/java/ru/max/lsn4_3_7/UntrustworthyMailWorker.java
@@ -5,7 +5,11 @@ package ru.max.lsn4_3_7;
  */
 public class UntrustworthyMailWorker implements MailService {
     private final MailService[] mailServices;
-    private final RealMailService realMailService = new RealMailService();
+    private final RealMailService realMailService;
+
+    {
+        realMailService = new RealMailService();
+    }
 
     public UntrustworthyMailWorker(MailService[] mailServices) {
         this.mailServices = mailServices;
@@ -17,14 +21,14 @@ public class UntrustworthyMailWorker implements MailService {
 
     @Override
     public Sendable processMail(Sendable mail) {
-//        Sendable sendable = mailServices[0].processMail(mail);
-//        for (int i = 1; i < mailServices.length; i++) {
-//            mailServices[i].processMail(sendable);
-//        }
-        for (MailService mailService :
-                mailServices) {
-            mailService.processMail(mail);
+        Sendable sendable = mailServices[0].processMail(mail);
+        for (int i = 1; i < mailServices.length; i++) {
+            sendable = mailServices[i].processMail(sendable);
         }
+//        for (MailService mailService :
+//                mailServices) {
+//            mailService.processMail(mailService.processMail(mail));
+//        }
         return getRealMailService().processMail(mail);
     }
 }
