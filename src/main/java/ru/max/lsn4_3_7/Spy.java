3c14e8a8267f457933240b2d61b699c36607deeb
Maxim Serov <maxim58r@gmail.com>
2020-09-18 15:01:25 +0300

    4.3.7 decided

--- src/main/java/ru/max/lsn4_3_7/Spy.java
+++ src/main/java/ru/max/lsn4_3_7/Spy.java
@@ -9,6 +9,7 @@ import java.util.logging.Logger;
 public class Spy implements MailService {
     private final Logger logger;
 
+
     public Spy(Logger logger) {
         this.logger = logger;
     }
@@ -18,10 +19,11 @@ public class Spy implements MailService {
 
         if (mail instanceof MailMessage) {
             if (mail.getFrom().equals("Austin Powers") || mail.getTo().equals("Austin Powers")) {
-                logger.log(Level.WARNING, "Detected target mail correspondence: from " + mail.getFrom() + " to " +
-                        mail.getTo() + " \"" + ((MailMessage) mail).getMessage());
+                logger.log(Level.WARNING, "Detected target mail correspondence: from {0} to {1}\"{2}\"",
+                        new String[]{mail.getFrom(), mail.getTo(), ((MailMessage) mail).getMessage()});
             } else {
-                logger.log(Level.INFO, "Usual correspondence: from " + mail.getFrom() + " to " + mail.getTo());
+                logger.log(Level.INFO, "Usual correspondence: from {0} to {1}",
+                        new String[]{mail.getFrom(), mail.getTo()});
             }
         }
         return mail;
