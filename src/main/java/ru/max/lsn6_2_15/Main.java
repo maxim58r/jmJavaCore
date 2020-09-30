d7985df5e14037cd020c132dac296eee62a42895
Maxim Serov <maxim58r@gmail.com>
2020-09-30 23:16:52 +0300

    6.2.15 add HashMap

--- src/main/java/ru/max/lsn6_2_15/Main.java
+++ src/main/java/ru/max/lsn6_2_15/Main.java
@@ -1,28 +1,40 @@
 package ru.max.lsn6_2_15;
 
 import java.io.*;
-import java.nio.CharBuffer;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        Reader rd;
-    }
-
-    public static Map<String, Long> getSalesMap(Reader reader) throws IOException {
-        StringBuilder sb = new StringBuilder();
+        Reader reader = null;
         try {
-            while (reader.read() != -1) {
-                sb.append(reader.read());
-
-            }
-        } catch (IOException e) {
+            reader = new FileReader("C:\\Users\\400103\\IdeaProjects\\jmJavaCore\\src\\main\\java\\ru\\max\\text.txt");
+        } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
+        System.out.println(getSalesMap(reader));
+    }
 
-        String str;
-//        str.getChars();
+    /*Алексей 3000
+    Дмитрий 9000
+    Антон 3000
+    Алексей 7000
+    Антон 8000*/
+
+    public static Map<String, Long> getSalesMap(Reader reader) {
+        Scanner sc = new Scanner(reader);
+        while (sc.hasNextLine()) {
+            String[]  str = sc.nextLine().split(" ");
+            Map<String,Long> map = new HashMap<>();
+            map.merge( );
+            for (var i : str) {
+                System.out.print(i);
+            }
+            System.out.println();
+            Map<String, Long> mp = new LinkedHashMap<>();
+        }
+        String s;
+        s.concat()
+
         return new HashMap<>();
     }
 }
