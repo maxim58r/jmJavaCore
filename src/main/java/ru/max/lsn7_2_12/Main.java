2ef1c4f480e8c46ad44f2e81e63e74d05f05bd7a
Maxim Serov <maxim58r@gmail.com>
2020-10-05 19:52:30 +0300

    7.1.9 performed

--- src/main/java/ru/max/lsn7_2_12/Main.java
+++ src/main/java/ru/max/lsn7_2_12/Main.java
@@ -19,7 +19,9 @@ public class Main {
                 .limit(10)
                 .map(stringLongEntry -> stringLongEntry.getKey())
                 .collect(Collectors.<String>toList());
-        System.out.println(lst);
+        for (String s : lst) {
+            System.out.println(s);
+        }
     }
 // Lorem! ipsum dolor sit amet, consectetur adipiscing elit. Sed sodales consectetur purus at faucibus. Donec mi quam, tempor vel ipsum non, faucibus suscipit massa. Morbi lacinia velit blandit tincidunt efficitur. Vestibulum eget metus imperdiet sapien laoreet faucibus. Nunc eget vehicula mauris, ac auctor lorem. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer vel odio nec mi tempor dignissim.
 }
